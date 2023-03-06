9513f8e832e9b1ce0b2a5f15e5090b70d67e8124
365support <86206374+365support@users.noreply.github.com>
2023-03-06 14:03:37 +0900

    chore: first setting

---
+++ domains/home/index.tsx
@@ -0,0 +1,5 @@
+const HomeView = () => {
+	return <div>Enter</div>;
+};
+
+export default HomeView;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyle from "@/styles/GlobalStyle";
+
+export const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<RecoilRoot>
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyle />
+				<Component {...pageProps} />
+				<ReactQueryDevtools initialIsOpen={false} />
+				<ToastContainer position="top-right" hideProgressBar={true} />
+			</QueryClientProvider>
+		</RecoilRoot>
+	);
 }
